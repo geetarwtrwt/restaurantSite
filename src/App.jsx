0ef1f61299c3be874769433c3e8c7cccb94cb990
Geeta rawat <rwtgeet6@gmail.com>
2024-11-03 16:53:11 +0530

    add all file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./component/NavBar";
+
+let App = () => {
+  return (
+    <>
+      <div className="bg-white2">
+        <NavBar />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let NavBar=()=>{
+  return(
+    <>
+      <nav>
+        <div className="container"></div>
+      </nav>
+    </>
+  )
+}
+export default NavBar;
